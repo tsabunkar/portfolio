df2d3dfc1c3cebefeeddaa610c631877e02f4041
Sabunkar Tejas Shailesh <tsabunkar@gmail.com>
2026-06-24 14:09:25 +0530

    updated toy projects url with sub-domain approach

--- src/data/footprint.js
+++ src/data/footprint.js
@@ -103,14 +103,18 @@ export const YOUTUBE_PLAYLISTS = [
 export const TOY_PROJECTS = [
   {
     name: "Batman Study",
-    url: "https://d2sp3xp56r8kh5.cloudfront.net/",
+    url: "https://batman-study.tsabunkar.com/",
   },
   {
     name: "HIV Aware",
-    url: "https://d59s5g9yk2nky.cloudfront.net/",
+    url: "https://hiv-aware.tsabunkar.com/",
   },
   {
     name: "Face Verification",
-    url: "https://d1gxr6cligqpav.cloudfront.net/",
+    url: "https://face-verify.tsabunkar.com/",
+  },
+  {
+    name: "Arch2Lead Interviewer",
+    url: "https://arch2lead-interviewer.tsabunkar.com/",
   },
 ];
